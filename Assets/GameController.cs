7d5b9a7a6816136722c8ecdca2f2ebbe7b62e7f2
celebrater <tkgm45nbchk@gmail.com>
2022-07-04 14:16:04 +0900

    deleting and adding the cube successfully.

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    
+    public GameObject cube;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cube.SetActive(false); // false == hidden
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            cube.SetActive(true); // true == unveil
+        }
+
+        if (Input.GetKeyDown(KeyCode.Delete))
+        {
+            cube.SetActive(false);
+        }
     }
 }

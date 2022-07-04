0366412e8de424a86629a0abf50d1d31e1a53bfb
celebrater <tkgm45nbchk@gmail.com>
2022-07-04 11:42:57 +0900

    until L24

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -13,6 +13,15 @@ public class Test : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float x = Input.GetAxis("Horizontal");
+        Debug.Log(x);
         
+        float y = Input.GetAxis("Vertical");
+        Debug.Log(y);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space is pressed.");
+        }
     }
 }
